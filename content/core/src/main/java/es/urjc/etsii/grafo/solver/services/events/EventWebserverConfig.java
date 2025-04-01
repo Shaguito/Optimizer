c46839646cd0db3d43f332fd1c116f1508e77c07
Shaguito <i.rubio.2022@alumnos.urjc.es>
2025-04-01 15:47:32 +0200

    prueba

--- content/core/src/main/java/es/urjc/etsii/grafo/solver/services/events/EventWebserverConfig.java
+++ content/core/src/main/java/es/urjc/etsii/grafo/solver/services/events/EventWebserverConfig.java
@@ -1,5 +1,6 @@
 package es.urjc.etsii.grafo.solver.services.events;
 
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
